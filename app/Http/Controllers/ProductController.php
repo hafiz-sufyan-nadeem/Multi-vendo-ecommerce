6c5b3278e4c6d50e4898987716cd3614a1c44cfc
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-04-19 19:50:20 +0500

    make create product file and add create form fields

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ProductController extends Controller
 {
-    //
+    public function index(){
+
+    }
+
+    public function create(Request $request)
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'description' => 'required',
+            'sale' => 'nullable',
+            'image' => 'nullable',
+            'category_id' => 'required'
+        ]);
+
+        $validated['vendor_id'] = auth()->id();
+
+        Product::create($validated);
+
+        return redirect('/products');
+    }
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,5 @@
 <form method="POST" action="/products">
     @csrf
-
     <input type="text" name="name" placeholder="Name">
     <input type="number" name="price" placeholder="Price">
     <input type="number" name="stock" placeholder="Stock">
