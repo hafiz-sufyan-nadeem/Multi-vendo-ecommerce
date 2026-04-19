2fe6a5d88ec32625ab3c3b1e277690cf58ff12bd
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-04-19 16:53:41 +0500

    add fields of products table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+   protected $fillable = [
+       'name',
+       'price',
+       'stock',
+       'description',
+       'sale',
+       'image',
+       'category_id',
+       'vendor_id'
+
+   ];
 }
--- database/migrations/2026_04_19_112950_create_products_table.php
+++ database/migrations/2026_04_19_112950_create_products_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->text('description')->nullable();
+            $table->boolean('sale')->default(false);
+            $table->string('image')->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('vendor_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
