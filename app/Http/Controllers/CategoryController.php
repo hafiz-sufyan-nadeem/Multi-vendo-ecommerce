1bbd0b0f771d305504cac50f38493f8951a3c49f
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-04-27 13:05:13 +0500

    add Category methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,5 +7,49 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    public function index()
+    {
+        return view('categories.index');
+    }
 
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'nullable',
+        ]);
+        Category::create($validated);
+        return redirect('/categories');
+    }
+
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $validated = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image' => 'nullable',
+        ]);
+        $category->update($validated);
+        return redirect('/categories');
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return redirect('/categories');
+    }
 }
