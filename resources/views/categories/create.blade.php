912a91746a1bc3202c997774f68174ad2e47c23e
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-04-28 21:09:30 +0500

    make create form for categories

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -9,17 +9,9 @@
 
     <input type="text" name="name" placeholder="Name">
     <textarea name="description"></textarea>
-    <input type="image" name="image" placeholder="Image">
+    <input type="file" name="image" placeholder="Image">
 
 
-
-    <select name="category_id">
-        @foreach($categories as $category)
-            <option value="{{ $category->id }}">
-                {{ $category->name }}
-            </option>
-        @endforeach
-    </select>
 
     <button type="submit">Add Category</button>
 </form>
